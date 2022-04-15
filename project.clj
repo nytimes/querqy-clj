ae21b5c48c1a64111b2a7b8fdb70652ec12a1cf4
Jeremiah Via <jeremiah.via@nytimes.com>
2022-04-15 14:22:59 -0700

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.nytimes/querqy-clj "0.1.0"
+(defproject com.nytimes/querqy-clj "0.1.1-SNAPSHOT"
   :description "Querqy in Clojure"
   :url "https://github.com/nytimes/querqy-clj"
   ;;:license {:name "Apache License", :url "http://www.apache.org/licenses/LICENSE-2.0"}
